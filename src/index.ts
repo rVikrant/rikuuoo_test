999af365bb38846c5e2bdf8ad4cd783bf69a768a
Aishwarya Kumar <aishwaryakumar@ABHISHEKs-MacBook-Pro.local>
2021-07-13 14:23:49 +0530

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import * as Koa from "koa";
+import * as Router from "koa-router";
+
+import * as logger from "koa-logger";
+import * as json from "koa-json";
+
+const app = new Koa();
+const router = new Router();
+
+//Middleware
+app.use(json());
+app.use(logger());
+
+router.get('/',async (ctx,next)=>{
+    console.log(ctx);
+    ctx.body = {msg:'hello world'};
+    await next();
+})
+
+//routes
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(3000,()=>{
+    console.log('koa started')
+})
+
